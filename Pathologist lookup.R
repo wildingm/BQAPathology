e992d61b9f5327a17da4b9f949af7da306b8c05b
Michael Wilding <Michael.Wilding@phe.gov.uk>
2018-12-13 14:45:56 +0000

    Skip empty row to fix mids CSV download issue

--- Pathologist lookup.R
+++ Pathologist lookup.R
@@ -78,7 +78,7 @@ CheckFile<-winDialog(type = "yesno", "Do you want to check a BQA csv file for ne
 
 while(CheckFile=="YES") {
   filessrc<-choose.files() 
-  checksource<-read.csv(filessrc, skip = 2)
+  checksource<-read.csv(filessrc, skip = 3)
   checksource<-checksource[-nrow(checksource),1:23] #remove last row (states end of data), and extra columns with calculated fields
   checksource<-separate(checksource,col = Primary.Sort.Value,into = PrimarySortIDHeadings, sep = "\\*") #expands the Primary.Sort.Value field 
   checksourceselected<-checksource[,c(1,match("Path_national_code",names(checksource)),15:35)]
